2553768146096c6e1ec9bf36a19151a6225552e8
alsanusi <malkautsars@gmail.com>
2020-07-04 22:28:06 +0700

    fix: add validation

--- app/Http/Controllers/PenceramahController.php
+++ app/Http/Controllers/PenceramahController.php
@@ -9,6 +9,12 @@ use Illuminate\Http\Request;
 class PenceramahController extends Controller
 {
     public function create(Request $request) {
+        
+        $this->validate($request, [
+            'nama' => 'required|string',
+            'status' => 'required|string'
+        ]);
+        
         $data = $request->all();
         $penceramahData = Penceramah::create($data);
 
